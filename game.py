2a241a7f9372e1c151289e204472f52643b3a857
JojoNeedsPizza <johannes.faizade@gmx.de>
2026-09-05 21:52:13 +0200

    Update game.py
    Spacebar Pressed Detection works

--- game.py
+++ game.py
@@ -5,10 +5,7 @@ import time
 
 while 1:
 
-    time.sleep(0.01)
-
 if msvcrt.khbit():
     key = msvcrt.getch()
-
-if ord(key) == 32:
-    print "Spacebar = Pressed"
+    if ord(key) == 32:
+        print "Spacebar = Pressed"
