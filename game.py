9df8d5212519c94979a42032c10246a21527c28d
JojoNeedsPizza <johannes.faizade@gmx.de>
2026-09-05 22:22:03 +0200

    Update game.py
    Added Main Menu def

--- game.py
+++ game.py
@@ -3,6 +3,15 @@ import time
 
 # PizzaClicker99
 
+Score = 0
+
+def mainmenu():
+    print "=======MAIN======="
+    print "| 1. Launch Game  |"
+    print "| 2. My Website   |"
+    print "| 3.    Exit      |"
+    print "==================="
+    
 while 1:
 
 if msvcrt.khbit():
